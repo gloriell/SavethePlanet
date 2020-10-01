2a7138e837422487e633c294e9bf9b216256447e
Radu Andrei Mihaila <radumihaila1@gmail.com>
2020-10-01 23:35:12 +0300

    am mai facut unele bug-fixuri

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-// const pictures = [
-//     'url(img/main-wolf-image.png)',
-//     'url(img/main-wolf-image-amber.jpg)',
-//     'url(img/main-wolf-image-black.jpg)'
-// ]
+//cod animatie
+const pictures = [
+    'url(img/main-wolf-image.png)',
+    'url(img/main-wolf-image-amber.jpg)',
+    'url(img/main-wolf-image-black.jpg)'
+]
 
-// let hero = document.querySelector('.hero');
-// let x = 0;
-
-
-// function heroChange() {
-//     hero.style.background = pictures[x];
-//     x = (x + 1) % pictures.length;
-//     hero.classList.toggle('transition')
-// }
-// setInterval(heroChange, 4000);
+let hero = document.querySelector('.hero');
+let x = 0;
 
 
+function heroChange() {
+    hero.style.background = pictures[x];
+    x = (x + 1) % pictures.length;
+    hero.classList.toggle('transition');
+}
+setInterval(heroChange, 4000);
 
 
 
+
+//cod hamburger menu
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar__responsive');
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('appear');
 
+})
+
+//cod dropdown story hamburger
+let story = document.getElementById('story');
+window.onload = function () {
+
+    story.addEventListener('click', () => {
+        document.getElementsByClassName('navbar__story')[0].classList.toggle('change');
+        document.getElementsByClassName('navbar__story')[1].classList.toggle('change');
+        document.getElementsByClassName('navbar__story')[2].classList.toggle('change');
+    })
+};
+
+//cod dropdown story navbar
+let storyDropdown = document.querySelector('.navbar__dropdown');
+let dropdown = document.querySelector('.dropdown')
+
+storyDropdown.addEventListener('click', () => {
+    dropdown.classList.toggle('falling');
 })
