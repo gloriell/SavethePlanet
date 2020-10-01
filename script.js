46e25e771a9bab89d9ee6e9eef7d88f535ecdf7c
Radu Andrei Mihaila <radumihaila1@gmail.com>
2020-10-01 19:55:15 +0300

    modificare finala

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-const pictures = [
-    'url(img/main-wolf-image.png)',
-    'url(img/main-wolf-image-amber.jpg)',
-    'url(img/main-wolf-image-black.jpg)'
-]
+// const pictures = [
+//     'url(img/main-wolf-image.png)',
+//     'url(img/main-wolf-image-amber.jpg)',
+//     'url(img/main-wolf-image-black.jpg)'
+// ]
 
-let hero = document.querySelector('.hero');
-let x = 0;
-hero.style.backgroundSize = "cover";
-
-function heroChange() {
-    hero.style.background = pictures[x];
-    x = (x + 1) % pictures.length;
-    hero.classList.toggle('transition')
-}
-setInterval(heroChange, 4000);
+// let hero = document.querySelector('.hero');
+// let x = 0;
 
 
+// function heroChange() {
+//     hero.style.background = pictures[x];
+//     x = (x + 1) % pictures.length;
+//     hero.classList.toggle('transition')
+// }
+// setInterval(heroChange, 4000);
 
 
 
-const hamburger = document.querySelector('.navbar__hamburger');
+
+
+const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar__responsive');
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('appear');
-
-
 
 })
